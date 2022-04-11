8865f511d42b5caf31883e5f274b0dc2382ca552
beta442 <grandoblivioniv@gmail.com>
2022-04-11 21:57:36 +0300

    task class init

---
+++ labs/lab_01/ScrumBoard/Task/ITask.cs
@@ -0,0 +1,12 @@
+﻿namespace ScrumBoard
+{
+    internal interface ITask
+    {
+        string GetDescription();
+        string GetName();
+        ulong GetPriority();
+        void Rename(string name);
+        void SetDescription(string description);
+        void SetPriority(uint priority);
+    }
+}
---
+++ labs/lab_01/ScrumBoard/Task/Task.cs
@@ -0,0 +1,59 @@
+﻿namespace ScrumBoard.Task
+{
+    internal class Task : ITask
+    {
+        private string _name;
+        private string _description;
+        private uint _priority;
+        public Task(string name)
+        {
+            _name = name;
+            _description = "";
+            _priority = 0;
+        }
+
+        public Task(string name, string description)
+        {
+            _name = name;
+            _description = description;
+            _priority = 0;
+        }
+
+        public Task(string name, string description, uint priority)
+        {
+            _name = name;
+            _description = description;
+            _priority = priority;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void Rename(string name)
+        {
+            _name = name;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        public void SetDescription(string description)
+        {
+            _description = description;
+        }
+
+        public ulong GetPriority()
+        {
+            return _priority;
+        }
+
+        public void SetPriority(uint priority)
+        {
+            _priority = priority;
+        }
+    }
+}
