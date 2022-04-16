df1b23553f91ab3099360b8316d80f7f6f415b18
beta442 <grandoblivioniv@gmail.com>
2022-04-16 22:12:39 +0300

    task tests: done

--- labs/lab_01/scrum_board_test/TaskTest.cs
+++ labs/lab_01/scrum_board_test/TaskTest.cs
@@ -8,9 +8,17 @@ namespace ScrumBoardTest
     public class TaskUnitTest
     {
         [Fact]
-        public void C()
+        public void Created_task_has_correct_properties()
         {
+            const string taskName = "TaskName";
+            const string taskDescription = "tASkDescRiptiOn";
+            const int taskPriority = 1000;
 
+            ITask task = MockTask(taskName, taskDescription, taskPriority);
+
+            Assert.True(task.GetName() == taskName);
+            Assert.True(task.GetDescription() == taskDescription);
+            Assert.True(task.GetPriority() == taskPriority);
         }
 
         private ITask MockTask(string taskName, string taskDescription, int taskPriority)
