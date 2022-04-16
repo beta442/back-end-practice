239708a1bcfa75e4a4318860971de67f7263a97b
beta442 <grandoblivioniv@gmail.com>
2022-04-16 22:06:04 +0300

    column test: done

--- labs/lab_01/scrum_board_test/ColumnTest.cs
+++ labs/lab_01/scrum_board_test/ColumnTest.cs
@@ -2,6 +2,7 @@
 
 using ScrumBoard;
 using ScrumBoard.Factory;
+using System;
 
 namespace ScrumBoardTest
 {
@@ -70,6 +71,7 @@ namespace ScrumBoardTest
             const int taskPriority = 10;
             const string taskName = "Task1";
             const string taskDescriprtion = "Task1's description";
+            const int expectedTaskNumber = 0;
             ITask task = MockTask(taskName, taskDescriprtion, taskPriority);
 
             taskColumn.AddTask(task);
@@ -78,6 +80,120 @@ namespace ScrumBoardTest
             Assert.True(taskColumn.GetPrioritedTaskMap().ContainsKey(taskPriority));
             Assert.True(taskColumn.GetPrioritedTaskCount(taskPriority) == 1);
             Assert.True(taskColumn.GetTaskCount() == 1);
+            Assert.True(taskColumn.GetTaskBy(taskPriority, expectedTaskNumber) == task);
+        }
+
+        [Fact]
+        public void Attempt_to_find_non_existing_task_fails()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+            const int taskPriority = 10;
+            const string taskName = "Task1";
+            const string taskDescriprtion = "Task1's description";
+            const int expectedTaskNumber = 0;
+            ITask task = MockTask(taskName, taskDescriprtion, taskPriority);
+            taskColumn.AddTask(task);
+
+            Assert.Throws<Exception>(() => taskColumn.GetTaskBy(1, expectedTaskNumber));
+            Assert.Throws<Exception>(() => taskColumn.GetTaskBy(taskPriority, -900));
+            Assert.Throws<Exception>(() => taskColumn.GetTaskBy(taskPriority, 15000));
+        }
+
+        [Fact]
+        public void Created_column_doesnt_contain_priorited_task()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+
+            Assert.True(taskColumn.GetPrioritedTaskCount(0) == -1);
+            Assert.True(taskColumn.GetPrioritedTaskIndex(0, "SomeName") == -1);
+        }
+
+        [Fact]
+        public void Renaming_column_renames_it()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+            const string expectedName = "newName";
+
+            taskColumn.Rename(expectedName);
+
+            Assert.True(taskColumn.GetName() == expectedName);
+        }
+
+        [Fact]
+        public void Removing_task_removes_it()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+            const int taskPriority = 10;
+            const string taskName = "Task1";
+            const string taskDescriprtion = "Task1's description";
+            const int expectedTaskNumber = 0;
+            ITask task = MockTask(taskName, taskDescriprtion, taskPriority);
+            taskColumn.AddTask(task);
+
+            taskColumn.RemoveTask(taskPriority, expectedTaskNumber);
+
+            Assert.True(taskColumn.GetTaskCount() == 0);
+            Assert.True(!taskColumn.HasColumnPrioritedTasks(taskPriority));
+            Assert.True(taskColumn.GetPrioritedTaskCount(taskPriority) == -1);
+            Assert.True(taskColumn.GetPrioritedTaskMap().Count == 0);
+        }
+
+        [Fact]
+        public void Attempt_to_remove_task_with_wrong_parameters_fails()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+            const int taskPriority = 10;
+            const int unexpectedTaskPriority = 15;
+            const string taskName = "Task1";
+            const string taskDescriprtion = "Task1's description";
+            const int expectedTaskNumber = 0;
+            const int unexpectedTaskNumber = 10;
+            ITask task = MockTask(taskName, taskDescriprtion, taskPriority);
+            taskColumn.AddTask(task);
+
+            Assert.Throws<Exception>(() => taskColumn.RemoveTask(unexpectedTaskPriority, expectedTaskNumber));
+            Assert.Throws<Exception>(() => taskColumn.RemoveTask(taskPriority, unexpectedTaskNumber));
+        }
+
+        [Fact]
+        public void Removing_priorited_task_list_in_column_removes_it()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+            const int taskPriority = 10;
+            const string taskName = "Task1";
+            const string taskDescriprtion = "Task1's description";
+            ITask task = MockTask(taskName, taskDescriprtion, taskPriority);
+            taskColumn.AddTask(task);
+
+            taskColumn.RemovePrioritedTaskListInColumn(taskPriority);
+
+            Assert.True(!taskColumn.HasColumnPrioritedTasks(taskPriority));
+            Assert.True(taskColumn.GetPrioritedTaskCount(taskPriority) == -1);
+            Assert.True(taskColumn.GetPrioritedTaskMap().Count == 0);
+        }
+
+        [Fact]
+        public void Removing_priorited_task_list_with_wrong_parameters_gives_no_effect()
+        {
+            const string taskColumnName = "TestColumn";
+            ITaskColumn taskColumn = MockColumn(taskColumnName);
+            const int taskPriority = 10;
+            const string taskName = "Task1";
+            const string taskDescriprtion = "Task1's description";
+            ITask task = MockTask(taskName, taskDescriprtion, taskPriority);
+            taskColumn.AddTask(task);
+
+            taskColumn.RemovePrioritedTaskListInColumn(-500);
+
+            Assert.True(taskColumn.HasColumnPrioritedTasks(taskPriority));
+            Assert.True(taskColumn.GetPrioritedTaskCount(taskPriority) == 1);
+            Assert.True(taskColumn.GetPrioritedTaskMap().Count == 1);
         }
 
         private ITaskColumn MockColumn(string title)
