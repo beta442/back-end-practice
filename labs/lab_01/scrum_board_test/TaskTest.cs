509d6a354b3639f0cba32e73a42e199b5d81a4e1
beta442 <grandoblivioniv@gmail.com>
2022-04-16 22:06:17 +0300

    task test init

---
+++ labs/lab_01/scrum_board_test/TaskTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+using ScrumBoard;
+using ScrumBoard.Factory;
+
+namespace ScrumBoardTest
+{
+    public class TaskUnitTest
+    {
+        [Fact]
+        public void C()
+        {
+
+        }
+
+        private ITask MockTask(string taskName, string taskDescription, int taskPriority)
+        {
+            return ScrumBoardFactory.CreateTask(taskName, taskDescription, taskPriority);
+        }
+    }
+}
